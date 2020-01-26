ff372a51afeef8a1acdb3cd46ff5f6382e516fb4
Spaceman <rionlion100@gmail.com>
2020-01-25 19:49:50 -0500

    add sounds and update ver

--- src/main/java/com/github/rionlion100/secretrooms.java
+++ src/main/java/com/github/rionlion100/secretrooms.java
@@ -8,22 +8,23 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class secretrooms implements ModInitializer {
 	public static final Item camo_paste = new Item(new Item.Settings().group(secretrooms.MAIN_GROUP));
-	public static final TorchLeverBlock torch_lever = new TorchLeverBlock(FabricBlockSettings.of(Material.WOOD).lightLevel(0).build());
+	public static final TorchLeverBlock torch_lever = new TorchLeverBlock(FabricBlockSettings.of(Material.WOOD).sounds(BlockSoundGroup.WOOD).lightLevel(0).build());
 	public static final SolidAirBlock solid_air = new SolidAirBlock(FabricBlockSettings.of(Material.AIR).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_oak = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_spruce = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_jungle = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_acacia = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_dark_oak = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_birch = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_cobble = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_stone = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
-	public static final OneWayGlassBlock one_way_glass_dirt = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_oak = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_spruce = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_jungle = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_acacia = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_dark_oak = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_birch = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_cobble = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_stone = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
+	public static final OneWayGlassBlock one_way_glass_dirt = new OneWayGlassBlock(FabricBlockSettings.of(Material.GLASS).hardness(.45f).sounds(BlockSoundGroup.GLASS).nonOpaque().build());
 
 	public static final ItemGroup MAIN_GROUP = FabricItemGroupBuilder.create(
 		new Identifier("secretrooms", "general"))
