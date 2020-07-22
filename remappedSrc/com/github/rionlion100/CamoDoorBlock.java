8f983976da923bf53243d4479de9ac2dfcde8628
Spaceman <rionlion100@gmail.com>
2020-07-22 17:58:30 -0400

    Remove files not needed

--- remappedSrc/com/github/rionlion100/CamoDoorBlock.java
+++
@@ -1,9 +0,0 @@
-package com.github.rionlion100;
-
-import net.minecraft.block.DoorBlock;
-
-class CamoDoorBlock extends DoorBlock {
-    protected CamoDoorBlock(Settings settings) {
-        super(settings);
-    }
-}
